2d2feda3d326a0463ca55a59de275c7998c0f01d
worraps <99826356+worraps@users.noreply.github.com>
2022-06-25 14:13:04 +0200

    Add files via upload

---
+++ crud.rb
@@ -0,0 +1,35 @@
+require 'bundler/inline'
+
+gemfile true do
+ source 'http://rubygems.org'
+ gem 'bcrypt'
+end
+
+module Crud
+  require 'bcrypt'
+  puts "Module CRUD activated"
+
+  def Crud.create_hash_digest(password)
+    BCrypt::Password.create(password)
+  end
+
+  def Crud.verify_hash_digest(password)
+    BCrypt::Password.new(password)
+  end
+
+  def Crud.create_secure_users(list_of_users)
+    list_of_users.each do |user_record|
+      user_record[:password] = create_hash_digest(user_record[:password])
+    end
+    list_of_users
+  end
+
+  def Crud.authenticate_user(username, password, list_of_users)
+    list_of_users.each do |user_record|
+      if user_record[:username] == username && verify_hash_digest(user_record[:password]) == password
+        return user_record
+      end
+    end
+    "Credentials were not correct"
+  end
+end
